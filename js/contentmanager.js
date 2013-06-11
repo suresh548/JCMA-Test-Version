867708ea3572bf1d16e20afb7da827103a36513b
Suresh Peddapu <suresh.peddapu@accenture.com>
2013-06-11 17:45:27 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -3940,7 +3940,7 @@ $("#cat_select_items_button").hide();
 */
 //$("#catFrom").css("margin-top","-250px");
 //$("#cat_from_space").css("margin-top","px");
-
+$("#actionLabelDiv").show();
 $("#selection_menu").hide();
 $("#finalCatShow").show();
 
