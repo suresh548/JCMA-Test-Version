dff90b8d8f21a778f39e14e19512026e70dafc03
Suresh Peddapu <suresh.peddapu@accenture.com>
2013-06-11 18:19:25 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -3911,7 +3911,6 @@ $("#catTo").show();
 }
 
 function startUpdatingCategories() {
-alert("start updating...");
 	//alert("catagory selectionn....");
 	//alert("browserName = "+browserName);
 
@@ -3941,6 +3940,7 @@ $("#cat_select_items_button").hide();
 //$("#catFrom").css("margin-top","-250px");
 //$("#cat_from_space").css("margin-top","px");
 $("#selection_menu").hide();
+$("#stylized").show();
 $("#finalCatShow").show();
 
 
